1d25eb7b6aa4f60f925091757cb9fdb1806c20fe
aldys002 <igidiaaldys@gmail.com>
2024-05-02 15:47:05 +0700

    menambah fungsi if

--- dll.cpp
+++ dll.cpp
@@ -17,5 +17,11 @@ void addNode() {
     cout << "\nEnter the roll number of the student: ";
     cin >> newNode->noMhs;      // Assign value to the data field of the new node
     cout << "\nEnter the name of the Student: ";
-    cin >> newNode->name;       // 
+    cin >> newNode->name;       // Assign value to the data field of the new node
+
+    // Insert the new Node in the list
+    if (START == NULL || newNode->noMhs <= START->noMhs) {
+
+        
+    }
 }
