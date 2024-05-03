335ba00db363651069db86f5aedd018f0d906fad
aldys002 <igidiaaldys@gmail.com>
2024-05-03 20:11:04 +0700

    membuat step 1.a

--- dll.cpp
+++ dll.cpp
@@ -34,4 +34,8 @@ void addNode() {
         newNode->prev = NULL;  //step 5 : make the new node point to NULL
         START = newNode; // step 6 : make the new node the first node
     }
+    else {
+        // Insert the new Node in the middle or at the end
+        Node *current = START; // step 1.a : start from the first node
+    }
 }
